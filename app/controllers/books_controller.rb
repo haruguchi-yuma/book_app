36a9a2f1e40765f85ea1ecf68dfc8330f18d9ea3
yuma <ikeoku.y@gmail.com>
2020-09-13 15:08:37 +0900

    Add paging functionally

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+    @books = Book.page(params[:page]).per(15)
   end
 
   # GET /books/1
